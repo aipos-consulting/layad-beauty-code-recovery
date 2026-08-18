6e33dbfd95a17f9c868ddf8391dd8e3aece73990
aipos-consulting <herriskim@naver.com>
2026-08-18 15:26:25 +0900

    security: lock search path for product identity functions

---
+++ supabase/migrations/20260818_secure_product_identity_functions.sql
@@ -0,0 +1,5 @@
+alter function public.normalize_product_name(text)
+  set search_path = pg_catalog, public;
+
+alter function public.set_product_normalized_name()
+  set search_path = pg_catalog, public;
